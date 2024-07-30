2b5d19ec9bb2fef98fd2f85a3d56052edf019f9f
MrDatfeel <mrrencover@gmail.com>
2024-07-30 13:04:22 +0300

    Add files via upload

---
+++ newsPortal/newsPortal/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from news import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  # Маршруты для панели администратора
+    path('', views.index, name='index'),  # Маршрут для главной страницы приложения news
+]
+
+
+
+
+
